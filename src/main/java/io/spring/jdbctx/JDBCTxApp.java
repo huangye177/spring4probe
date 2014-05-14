9ba812880ce80c01091ea5a6c051d0198ffe09e3
Ye Huang <ye.huang@itc-engineering.com>
2014-05-14 14:32:46 +0200

    init jdbc-tx support

---
+++ src/main/java/io/spring/jdbctx/JDBCTxApp.java
@@ -0,0 +1,6 @@
+package io.spring.jdbctx;
+
+public class JDBCTxApp
+{
+
+}
---
+++ src/main/java/io/spring/jdbctx/JDBCTxConfig.java
@@ -0,0 +1,9 @@
+package io.spring.jdbctx;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JDBCTxConfig
+{
+
+}
