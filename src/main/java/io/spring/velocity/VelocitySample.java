075c5e5bc64b9d8a62f651846f4d90b241d99043
Ye Huang <yehuang@de-gn-19572.eng.sophos>
2014-06-09 18:58:37 +0200

    more features on velocity

--- src/main/java/io/spring/velocity/VelocitySample.java
+++ src/main/java/io/spring/velocity/VelocitySample.java
@@ -1,6 +1,8 @@
 package io.spring.velocity;
 
 import java.io.StringWriter;
+import java.util.*;
+
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
@@ -21,9 +23,29 @@ public class VelocitySample {
         /*  next, get the Template  */
         Template t = ve.getTemplate( "src/main/resources/helloworld.vm" );
 
+        /* create our list of maps  */
+        ArrayList list = new ArrayList();
+
+        Map map = new HashMap();
+        map.put("name", "horse");
+        map.put("price", "00.00");
+        list.add( map );
+
+        map = new HashMap();
+        map.put("name", "dog");
+        map.put("price", "9.99");
+        list.add( map );
+
+        map = new HashMap();
+        map.put("name", "bear");
+        map.put("price", ".99");
+        list.add( map );
+
         /*  create a context and add data */
         VelocityContext context = new VelocityContext();
+
         context.put("name", "World");
+        context.put("petList", list);
 
         /* now render the template into a StringWriter */
         StringWriter writer = new StringWriter();
